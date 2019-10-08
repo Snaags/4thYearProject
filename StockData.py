c0401236c18184e76fa20bcce51d9975b3239fec
Snaags <55416240+Snaags@users.noreply.github.com>
2019-10-08 19:18:46 +0100

    Added binary file type

--- StockData.py
+++ StockData.py
@@ -2,27 +2,31 @@
 
 
 
-##Gets historical Stock data
-def stockhist(TAG,startDate,endDate):
+##Gets historical Stock data from yahoo finance and saves as .csv file
+def StockHist(TAG,startDate,endDate):
 
 	import yfinance as yf
 	import os
 	import pandas
-	
+	import numpy as np
 	path = os.getcwd()
 	os.chdir(path+"\\StockData")
 
-	str(TAG)
 	##Getting Data
-	data = yf.Ticker(TAG)
+	data = yf.Ticker(str(TAG))
 	hist = data.history(start = startDate, end = endDate)
-	
 
 	##Writing file
-	filename = str(TAG+".csv")
-	pandas.DataFrame.to_csv(hist,filename)
+	pandas.DataFrame.to_csv(hist,str(TAG+".csv"))
 
-stockhist("GOOGL","2018-01-01","2018-12-31")
+	#Writes to binary numpy file type
+	formattedData = hist.to_records()
+	formattedData.tofile(str(TAG)+".npy")
+
+
+StockHist("GOOGL","2018-01-01","2018-12-31")
+
+
 
 
 
